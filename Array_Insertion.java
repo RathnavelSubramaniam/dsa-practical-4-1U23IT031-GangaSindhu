aa4e498d1cef1b4ccb5911b8fb45d844d584cb3e
1U23IT031-GangaSindhu <140485031+1U23IT031-GangaSindhu@users.noreply.github.com>
2025-01-23 01:08:18 +0000

    Done 4

--- Array_Insertion.java
+++ Array_Insertion.java
@@ -19,9 +19,18 @@ public class Array_Insertion {
         System.out.println("Enter the element to be inserted");
         e=sc.nextInt();
         arrayInsert(arr,p,e,len);
+        sc.close();
     }
-    static void arrayInsert()
+    static void arrayInsert(int arr[], int p, int e, int len)
     {
-    //TYPE OF CODE HERE
+      for(int i=len-1; i>=p-1; i--){
+        arr[i+1] = arr[i];
+      }
+      arr[p-1] = e;
+      System.out.println("After Inserting");
+      System.out.println("Length of the array:" + (len + 1));
+      for(int i=0; i<len+1; i++){
+        System.out.println("arr["+i+"]=+" + arr[i]);
+      }
     }
 }
